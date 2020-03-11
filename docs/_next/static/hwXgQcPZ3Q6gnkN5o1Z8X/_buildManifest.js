f2d0a23ec67fd984b3887825ff54f394c5eedc9c
aaronm <moradi.aaron@gmail.com>
2020-03-11 00:25:45 -0400

    static site again

---
+++ docs/_next/static/hwXgQcPZ3Q6gnkN5o1Z8X/_buildManifest.js
@@ -0,0 +1,6 @@
+self.__BUILD_MANIFEST = (function(a) {
+  return { "/": [a], "/documentation": [a], "/index": [a] };
+})(
+  "static\u002Fchunks\u002F649a73727429664de9bc4b0d02d8f70cb79f14e4.3cde86cf196d721b5f3d.js"
+);
+self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
